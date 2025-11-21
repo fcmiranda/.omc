2272b70360a262446e2728332721f2ef5555ca4b
Felipe <fecmbr@gmail.com>
2025-11-21 09:20:51 -0300

    feat: Update waybar-restart alias for improved functionality

--- .zsh/utils/aliases.zsh
+++ .zsh/utils/aliases.zsh
@@ -1,4 +1,4 @@
-alias waybar-restart="pkill waybar && waybar &"
+alias waybar-restart='killall waybar && hyprctl dispatch exec waybar'
 alias reload="source ~/.zshrc"
 alias omc='/usr/bin/git --git-dir=/home/fecavmi/.omc --work-tree=/home/fecavmi'
 alias code.omc='GIT_DIR=/home/fecavmi/.omc GIT_WORK_TREE=/home/fecavmi code /home/fecavmi'
