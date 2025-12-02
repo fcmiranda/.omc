b7a95306540e43738c93994cea73e830a7eaa182
felipe cavalcante miranda <fecmbr@gmail.com>
2025-12-01 23:14:45 -0300

    feat: Replace local aliases and functions with symlinks to dotfiles

--- .zsh/utils/aliases.zsh
+++ .zsh/utils/aliases.zsh
@@ -1,5 +1,1 @@
-alias waybar-restart='killall waybar && hyprctl dispatch exec waybar'
-alias reload="source ~/.zshrc"
-alias omc='/usr/bin/git --git-dir=/home/fecavmi/.omc --work-tree=/home/fecavmi'
-alias code.omc='GIT_DIR=/home/fecavmi/.omc GIT_WORK_TREE=/home/fecavmi code /home/fecavmi'
-alias code.here='GIT_DIR=$(pwd) GIT_WORK_TREE=$(pwd) code $(pwd)'
+../../.dotfiles/zsh/.zsh/utils/aliases.zsh
